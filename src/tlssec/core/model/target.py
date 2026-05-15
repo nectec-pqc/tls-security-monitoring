b84c08e305999550b7387be663e18389019ee50b
Apiwat Chantawibul <billiska@gmail.com>
2026-05-15 18:51:43 +0700

    customize ID as generated identity primary key
    Part of #127

--- src/tlssec/core/model/target.py
+++ src/tlssec/core/model/target.py
@@ -1,11 +1,13 @@
 from sqlmodel import Field, Relationship
+from sqlalchemy import Column, Integer, Identity
+
 
 from tlssec.database.sqlmodel import SQLModel
 
 class Target(SQLModel):
     id: int | None = Field(
         default = None,
-        primary_key = True,
+        sa_column = Column(Integer, Identity(always = True), primary_key = True),
     )
     hostname: str = Field(
         index = True,
