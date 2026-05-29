238b69a1020b0062413ea3ae1bd2b883d16fab8b
Tana <tanakitjaroenamaritchok@gmail.com>
2026-05-29 10:25:04 +0700

    Normallized style

--- src/tlssec/core/model/testssl_result.py
+++ src/tlssec/core/model/testssl_result.py
@@ -8,69 +8,70 @@ from tlssec.database.sqlmodel import SQLModel
 
 
 class EndPoint(SQLModel):
-    endPointID: int | None = Field(
+    id : int | None = Field(
         default = None,
         sa_column=Column(Integer, Identity(always=True), primary_key=True),
      ) 
-    partOfService: int = Field(
+    part_of_service: int = Field(
         foreign_key = "service.serviceID",
         index = True,
     )
     scheme: str = Field(
         description="What protocol/encryption they handel",
     )
-    hostName: str
+    hostname: str
     IPv4: str # in postgrase they have inet and cidr for optimzed store ip let look lather
     port: int
     path: str = Field(
         description="testssl do scan at connection level, so URL is inrelavant",
     )
-    validAfter: datetime # use to track date that this end point start to alive 
-    validBefore: datetime # use to track date that this end poin die
+    valid_after: datetime # use to track date that this end point start to alive 
+    valid_before: datetime # use to track date that this end poin die
 
 class Scan(SQLModel):
-    scanID: int | None = Field(
+    id : int | None = Field(
         default=None,
-        sa_column=Column(Integer, Identity(always=True), primary_key=True)
+        sa_column=Column(Integer, Identity(always=True), primary_key=True),
     )
-    endPointID: int = Field(
+    end_point_id: int = Field(
         index=True,
-        foreign_key="end_point.endPointID"
+        foreign_key="end_point.endPointID",
     )
     param: dict | None = Field(
         default = None,
         sa_type = JSONB,
-        description = "How testssl got call, option and arguemnt supply to testssl"
+        description = "How testssl got call, option and arguemnt supply to testssl",
     )
     result: dict | list = Field(sa_type=JSONB)
     start: datetime
-    timeTaken: int
+    time_taken: int
 
 class Service(SQLModel):
-    serviceID: int | None = Field(
+    id: int | None = Field(
         default = None,
-        sa_column=Column(Integer, Identity(always=True), primary_key=True)
+        sa_column=Column(Integer, Identity(always=True), primary_key=True),
     )
 
 class Service_Tag_Map(SQLModel): 
-    serviceID: int  = Field(
+    service_id: int  = Field(
         primary_key=True,
-        foreign_key="service.serviceID"
+        foreign_key="service.serviceID",
     )
-    tagID: int = Field(
+    tag_id: int = Field(
         primary_key=True,
-        foreign_key="service_tag.tagID"
+        foreign_key="service_tag.tagID",
     )
 
 class ServiceTag(SQLModel):
-    tagID: int | None = Field(
+    tag_id: int | None = Field(
         default = None,
-        sa_column=Column(Integer, Identity(always=True), primary_key=True)
+        sa_column=Column(Integer, Identity(always=True), primary_key=True),
     )
-    parentID: int | None = Field(
+    parent_id: int | None = Field(
         default=None,
-        foreign_key="service_tag.tagID"  
+        foreign_key="service_tag.tagID", 
     )
     name: str
     description: str | None = None
+
 
