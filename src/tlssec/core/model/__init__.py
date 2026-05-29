1aa3b356b6ef461d10586c89bf4574ce665b6215
Tana <tanakitjaroenamaritchok@gmail.com>
2026-05-29 10:45:58 +0700

    add class and they table to __init__

--- src/tlssec/core/model/__init__.py
+++ src/tlssec/core/model/__init__.py
@@ -3,3 +3,4 @@ from .service import (
 )
 from .target import Target, TargetTable
 from .scan import Scan, ScanTable
+from .testssl_result import EndPoint, EndPointTable, Scan, ScanTable, Service, ServiceTable, ServiceTag, ServiceTagTable, ServiceTagMap, ServiceTagMapTable
