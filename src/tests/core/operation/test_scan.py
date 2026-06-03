9346db1ed0a57e633823783b6aa9451b50310f30
Apiwat Chantawibul <billiska@gmail.com>
2026-06-03 23:21:18 +0700

    add a timeout, don't let test hang

--- src/tests/core/operation/test_scan.py
+++ src/tests/core/operation/test_scan.py
@@ -4,14 +4,25 @@ import pytest
 
 
 def test_call_testssl():
-    async def call():
+    async def call() -> str | None:
         proc = await asyncio.create_subprocess_exec(
             'testssl', '--help',
             stdout = asyncio.subprocess.PIPE,
             stderr = asyncio.subprocess.PIPE,
         )
-        out, err = await proc.communicate()
-        return out.decode()
+        try:
+            out, err = await asyncio.wait_for(
+                proc.communicate(),
+                timeout = 1,
+            )
+            return out.decode()
+        except TimeoutError:
+            try:
+                proc.kill()
+            except OSError:
+                pass
+            await proc.wait()
+        return
 
     result = asyncio.run(call())
     assert 'testssl [options] <URI>' in result
