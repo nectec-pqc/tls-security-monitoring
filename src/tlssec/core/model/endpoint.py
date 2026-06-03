adbe5121400af71567733341a91be9b34a7520fb
Tana <tanakitjaroenamaritchok@gmail.com>
2026-06-03 16:30:38 +0700

    change end point data model to use sql model

--- src/tlssec/core/model/endpoint.py
+++ src/tlssec/core/model/endpoint.py
@@ -1,59 +1,29 @@
 from datetime import datetime
 from typing import Optional
 
-from sqlalchemy import (
-        Column,
-        Integer,
-        Identity,
-        String,
-        UniqueConstraint,
-        ForeignKey,
-        DateTime,
-    )
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Relationship
+from sqlmodel import Field, Relationship
+from sqlalchemy import Column, Integer, Identity, UniqueConstraint
 
+from tlssec.database.sqlmodel import SQLModel  
 
-class Base(DeclarativeBase):
-    pass
-
-
-class EndPoint(Base):
-
-    __tablenmae__ = "end_point"
-
-    id: Mapped[int] = mapped_column(
-        Integer,
-        Identity(always=True),
-        primary_key=True,
-    )
-    target_id: Mapped[int] = mapped_column(
-        Integer,
-        ForeignKey("target.id"),
-        index=True,
-        nullable=False,
-    )
-    hostname: Mapped[str] = mapped_column(String(255), nullable=False)
-    port: Mapped[int] = mapped_column(Integer, nullable=False)
-    protocol: Mapped[str] = mapped_column(String(50), default="tcp", nullable=False)
-    first_seen: Mapped[datetime] = mapped_column(DateTime, nullable=False)
-    last_seen: Mapped[datetime] = mapped_column(DateTime, nullable=False)
-    retired_at: Mapped[Optional[datetime]] = mapped_column(
-        DateTime,
-        nullable=True,
-        index=True,
+class EndPoint(SQLModel):
+    id: int | None = Field(
         default=None,
+        sa_column=Column(Integer, Identity(always=True), primary_key=True),
     )
-
-    # Relationships
-    target: Mapped[Optional["Target"]] = relationship(
-        "Target",
-        back_populates="endpoints",
-    )
-    scans: Mapped[list["Scan"]] = relationship(
-        "Scan",
-        back_populates="endpoint",
-    )
+    target_id: int = Field(foreign_key='target.id', index=True)
+    hostname: str = Field(max_length=253)
+    port: int
+    protocol: str = Field(default='tcp')
+    first_seen: datetime
+    last_seen: datetime
+    retired_at: datetime | None = Field(default=None, index=True)
 
     __table_args__ = (
-        UniqueConstraint("target_id", "hostname", "port", "protocol"),
+        UniqueConstraint('target_id', 'hostname', 'port', 'protocol'),
     )
+
+
+class EndPointTable(EndPoint, table=True):
+    target: Optional['TargetTable'] = Relationship(back_populates='endpoints')
+    scans: list['ScanTable'] = Relationship(back_populates='scans')
