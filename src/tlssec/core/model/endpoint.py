88304b6d7b5cf3764ce6aad20015bebf7b24bc48
Tana <tanakitjaroenamaritchok@gmail.com>
2026-06-04 13:16:45 +0700

    New end point #9

--- src/tlssec/core/model/endpoint.py
+++ src/tlssec/core/model/endpoint.py
@@ -1,10 +1,12 @@
 from datetime import datetime
-from typing import Optional
+from typing import Optional, Literal
 
 from sqlmodel import Field, Relationship
 from sqlalchemy import Column, Integer, Identity, UniqueConstraint
 
 from tlssec.database.sqlmodel import SQLModel  
+
+Protocol = Literal['tcp', 'udp', 'http', 'https']
 
 class EndPoint(SQLModel):
     id: int | None = Field(
@@ -13,17 +15,16 @@ class EndPoint(SQLModel):
     )
     part_of_service_id: int = Field(foreign_key='service.id', index=True)
     hostname: str = Field(max_length=253)
-    port: int
-    part: str 
-    protocol: str = Field(default='tcp')
+    port: int = Field(ge=1)
+    part: str = Field(default='/') 
+    protocol: Protocol = 'tcp'
     first_seen: datetime
     last_seen: datetime
     retire_at: datetime | None = Field(default=None) 
 
     __table_args__ = (
-        UniqueConstraint('part_of_serviceID', 'hostname', 'port', 'protocol'),
+        UniqueConstraint('part_of_service_id', 'hostname', 'port', 'protocol', 'path'),
     )
-
 
 class EndPointTable(EndPoint, table=True):
     target: Optional['ServiceTable'] = Relationship(back_populates='endpoints')
