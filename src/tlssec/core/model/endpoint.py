c62b6a5484d6aae5d705bfc0b641ff2d3e6ab5de
Tana <tanakitjaroenamaritchok@gmail.com>
2026-06-11 16:01:18 +0700

    First look at new endpoint model

--- src/tlssec/core/model/endpoint.py
+++ src/tlssec/core/model/endpoint.py
@@ -9,7 +9,6 @@ from pydantic import (
         Field as PydanticField,
 )
 from sqlalchemy import (
-        Column, 
         Integer, 
         Identity, 
         UniqueConstraint, 
@@ -34,4 +33,53 @@ class Protocol(str, Enum):
     https = 'https'
 
 
+class Endpoint(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
 
+    id: int | None = None
+    part_of_service_id: int
+    ip: IPvAnyAddress | None = None
+    hostname: str = PydanticField(
+        min_length=1, 
+        max_length=253,
+    )
+    port: int = PydanticField(
+        default=443, 
+        ge=1, 
+    )
+    path: UrlPath = '/'
+    protocol: Protocol = Protocol.https
+    first_seen: datetime
+    last_seen: datetime
+    retire_at: datetime | None = None
+
+class EndpointTable(Base):
+    __tablename__ = 'endpoint'
+
+    id: Mapped[int] = mapped_column(
+        Integer, 
+        Identity(always=True), 
+        primary_key=True,
+    )
+    part_of_service_id: Mapped[int] = mapped_column(
+        ForeignKey('service.id'), 
+        index=True,
+    )
+    ip: Mapped[Optional[str]] = mapped_column(
+            InetType, 
+            nullable=True,
+    )
+    hostname: Mapped[str] = mapped_column(String(253))
+    port: Mapped[int] = mapped_column(
+            Integer, 
+            default=443,
+    )
+    path: Mapped[str] = mapped_column(
+            String, 
+            default='/',
+    )
+    protocol: Mapped[str] = mapped_column(
+            String(10), 
+            default=Protocol.https.value,
+    )
+    first_seen: Mapped[datetime] = mapped_column(DateTime(timezone=True))
