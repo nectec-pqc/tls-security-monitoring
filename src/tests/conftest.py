e788aabe2969d3cc5b0d712f57d306baa95bad94
Apiwat Chantawibul <billiska@gmail.com>
2026-08-29 11:17:07 +0700

    move ssh server fixture to shared conftest
    - change fixture name to current_openssh_server
      to explicitly match software name, not just protocol name.

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,5 +1,6 @@
 import subprocess
 from pathlib import Path
+from textwrap import dedent
 
 import pytest
 from sqlalchemy.orm import Session
@@ -107,6 +108,63 @@ def current_openssl_server(cache_dir):
     # TODO: add timeout. Need to use asyncio?
     for line in proc.stdout:
         if line == b'ACCEPT\n':
+            break
+    yield proc
+    try:
+        proc.terminate()
+        proc.wait(timeout = 1)
+    except subprocess.TimeoutExpired:
+        with proc:
+            proc.kill()
+
+
+@pytest.fixture(scope = 'module')
+def current_openssh_server(cache_dir):
+    server_config_dir = (cache_dir / 'current_openssh_server').resolve()
+    server_config_dir.mkdir(parents = True, exist_ok = True)
+
+    private_key_path = server_config_dir / 'ssh_host_ed25519_key'
+    private_key_path.unlink(missing_ok = True)
+
+    public_key_path = private_key_path.with_suffix('.pub')
+    public_key_path.unlink(missing_ok = True)
+
+    subprocess.run(
+        [
+            'ssh-keygen',
+            '-t', 'ed25519',
+            '-f', 'ssh_host_ed25519_key',
+            '-N', '',
+        ],
+        cwd = server_config_dir,
+        check = True,
+        timeout = 1,
+    )
+
+    sshd_config_path = server_config_dir / 'sshd_config'
+    sshd_config_path.unlink(missing_ok = True)
+
+    with open(sshd_config_path, 'w') as f:
+        f.write(dedent(f"""
+            Port 2222
+            ListenAddress 127.0.0.1
+            PidFile {server_config_dir / 'sshd.pid'}
+            HostKey {server_config_dir / 'ssh_host_ed25519_key'}
+            PasswordAuthentication yes
+            PermitRootLogin no
+        """))
+
+    proc = subprocess.Popen(
+        [
+            '/usr/sbin/sshd', '-D', '-e',
+            '-f', str(sshd_config_path),
+        ],
+        cwd = server_config_dir,
+        stdout = subprocess.DEVNULL,
+        stderr = subprocess.PIPE,
+    )
+    for line in proc.stderr:
+        if line.startswith(b'Server listening on'):
             break
     yield proc
     try:
--- src/tests/core/ssh_audit/test_ssh_audit.py
+++ src/tests/core/ssh_audit/test_ssh_audit.py
@@ -1,68 +1,10 @@
 import subprocess
 from pathlib import Path
-from textwrap import dedent
 
 import pytest
 import ssh_audit.exitcodes
 
 from tlssec.core.ssh_audit import SshAudit
-
-
-@pytest.fixture(scope = 'module')
-def current_ssh_server(cache_dir):
-    server_config_dir = (cache_dir / 'current_ssh_server').resolve()
-    server_config_dir.mkdir(parents = True, exist_ok = True)
-
-    private_key_path = server_config_dir / 'ssh_host_ed25519_key'
-    private_key_path.unlink(missing_ok = True)
-
-    public_key_path = private_key_path.with_suffix('.pub')
-    public_key_path.unlink(missing_ok = True)
-
-    subprocess.run(
-        [
-            'ssh-keygen',
-            '-t', 'ed25519',
-            '-f', 'ssh_host_ed25519_key',
-            '-N', '',
-        ],
-        cwd = server_config_dir,
-        check = True,
-        timeout = 1,
-    )
-
-    sshd_config_path = server_config_dir / 'sshd_config'
-    sshd_config_path.unlink(missing_ok = True)
-
-    with open(sshd_config_path, 'w') as f:
-        f.write(dedent(f"""
-            Port 2222
-            ListenAddress 127.0.0.1
-            PidFile {server_config_dir / 'sshd.pid'}
-            HostKey {server_config_dir / 'ssh_host_ed25519_key'}
-            PasswordAuthentication yes
-            PermitRootLogin no
-        """))
-
-    proc = subprocess.Popen(
-        [
-            '/usr/sbin/sshd', '-D', '-e',
-            '-f', str(sshd_config_path),
-        ],
-        cwd = server_config_dir,
-        stdout = subprocess.DEVNULL,
-        stderr = subprocess.PIPE,
-    )
-    for line in proc.stderr:
-        if line.startswith(b'Server listening on'):
-            break
-    yield proc
-    try:
-        proc.terminate()
-        proc.wait(timeout = 1)
-    except subprocess.TimeoutExpired:
-        with proc:
-            proc.kill()
 
 
 # TODO: add success cases
@@ -93,7 +35,7 @@ def current_ssh_server(cache_dir):
 )
 @pytest.mark.regen_case
 async def test_generate_ssh_audit_json(
-    current_ssh_server,
+    current_openssh_server,
     name,
     target,
     expected_exitcode,
